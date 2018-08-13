137c088a6d63aed4d013513a96d76ec529690d8b
John Steill <jsteill@morgridge.org>
2018-08-13 15:27:00 -0500

    tuned the thresh to get 6/7 genes

--- LPWC_Analysis.R
+++ LPWC_Analysis.R
@@ -1,38 +1,39 @@
 library(ggplot2)
 source('LPWC_Analysis_Utilities.R')
 
-EC_DATA_FILE = 'Data/Sub_391_2_pool_22adb_hg19_SOX2_plus__foregut_endoderm__32a860ee85db9ac9/genes.no_mt.ec.tab' 
-TPM_DATA_FILE = 'Data/Sub_391_2_pool_22adb_hg19_SOX2_plus__foregut_endoderm__32a860ee85db9ac9/genes.no_mt.tpm.rescale.tab'
+DATA_DIR <- 'Data/Sub_391_2_pool_22adb_hg19_SOX2_plus__foregut_endoderm__32a860ee85db9ac9'
+EC_DATA_FILE <- file.path(DATA_DIR, 'genes.no_mt.ec.tab')
+TPM_DATA_FILE <- file.path(DATA_DIR, 'genes.no_mt.tpm.rescale.tab')
 
-if (file.exists('wkspace_lo.RData')){
-    load(file='wkspace_lo.RData')
-}
-if (!("sol_ec_hi" %in% ls())){
-    high_var_data = get_high_var_genes(infile=EC_DATA_FILE, sd_cv_thresh=500)
-    c(sol_ec_hi,dist_ec_hi,clust_ec_hi)=run_hi_lpwc(dat=high_var_data, 
+# Used to discover parameters of 175 & 40
+# dat = tuner(dataFile=EC_DATA_FILE, sd_cv_thresh=175)
+# dat = tuner(dataFile=TPM_DATA_FILE, sd_cv_thresh=40)
+
+high_var_ec_data = get_high_var_genes(dataFile=EC_DATA_FILE, sd_cv_thresh=175)
+high_var_tpm_data = get_high_var_genes(dataFile=TPM_DATA_FILE, sd_cv_thresh=40)
+
+c(sol_ec_hi,dist_ec_hi,clust_ec_hi)=run_hi_lpwc(dat=high_var_ec_data, 
                                                 timepoints = 0:96)
-}
-save.image(file='wkspace_lo.RData')
-save.image(file=paste0("wkspace_lo.RDatadata_set.csv",
+save.image(file='wkspace_all')
+save.image(file=paste0("wkspace_all.RData_",
                        format(Sys.time(), "%Y%m%d_%H%M%S_")))
-if (!("sol_tpm_hi" %in% ls())){
-    high_var_data = get_high_var_genes(infile=TPM_DATA_FILE, sd_cv_thresh=500)
-    c(sol_tpm_hi,dist_tpm_hi,clust_tpm_hi)=run_hi_lpwc(dat=high_var_data, 
+
+c(sol_tpm_hi,dist_tpm_hi,clust_tpm_hi)=run_hi_lpwc(dat=high_var_tpm_data, 
                                                        timepoints = 0:96)
-}
-save.image(file='wkspace_lo.RData')
-save.image(file= paste0( "wkspace_lo.RDatadata_set.csv",
+save.image(file='wkspace_all')
+save.image(file= paste0( "wkspace_all.RData_",
                         format(Sys.time(), "%Y%m%d_%H%M%S_")))
-if (!("sol_ec_lo" %in% ls())){
-    c(sol_ec_lo,dist_ec_lo,clust_ec_lo)=run_lo_lpwc(dat=high_var_data, 
+
+#if (!("sol_ec_lo" %in% ls())){
+c(sol_ec_lo,dist_ec_lo,clust_ec_lo)=run_lo_lpwc(dat=high_var_ec_data, 
                                                 timepoints = 0:96)
-}
-save.image(file='wkspace_lo.RData')
-save.image(file= paste0( "wkspace_lo.RDatadata_set.csv",
+#}
+save.image(file='wkspace_all.RData')
+save.image(file= paste0( "wkspace_all.RData_",
                         format(Sys.time(), "%Y%m%d_%H%M%S_")))
-if (!("sol_tpm_lo" %in% ls())){
-    c(sol_tpm_lo,dist_tpm_lo,clust_tpm_lo)=run_lo_lpwc(dat=high_var_data, 
+#if (!("sol_tpm_lo" %in% ls())){
+    c(sol_tpm_lo,dist_tpm_lo,clust_tpm_lo)=run_lo_lpwc(dat=high_var_tpm_data, 
                                                 timepoints = 0:96)
-}
-save.image(file= paste0( "wkspace_lo.RDatadata_set.csv",format(Sys.time(), "%Y%m%d_%H%M%S_")))
-save.image(file='wkspace_lo.RData')
+#}
+save.image(file= paste0( "wkspace_all.RData_",format(Sys.time(), "%Y%m%d_%H%M%S_")))
+save.image(file='wkspace_all.RData')
--- LPWC_Analysis_Utilities.R
+++ LPWC_Analysis_Utilities.R
@@ -4,7 +4,7 @@ library(LPWC)
 get_high_var_genes <- function(dataFile, sd_cv_thresh=500){
     # The true coefficient of variance is unitless, 
     # I squared the sd to get larger counts. 
-    my_data = read.table(dataFile, header=TRUE, row.names=1, sep = '\t')
+    my_data = read.table(dataFile, header=TRUE, row.names=1, sep = '\t',  quote = "", comment.char = "")
     my_data = my_data[,-ncol(my_data)] # Drop description
     my_data$sd_coef_var = apply(my_data, 1, function(x) sd(x) * sd(x)/ mean(x))
     my_data$sd_coef_var[is.na(my_data$sd_coef_var)]<-0
@@ -26,13 +26,10 @@ get_appropriate_neighbors <- function(t, degree){
 
 get_imputed_value <- function(data_row, time, degree){
     data_t = get_appropriate_neighbors(time, degree)
-    
 }
 
 downsample_equally_spaced_data <- function(data, n_timepoints){
     degree = int(ncol(data)/n_timepoints) + 1
-    
-    
 }
 
 run_hi_lpwc <- function(dat, timepoints){ 
@@ -57,30 +54,30 @@ run_lo_lpwc <- function(dat, timepoints){
     return (list(solution=solution, dist=dist, clust=clust))
 }
 
-if (!interactive()){
-    if (file.exists('wkspace_lo.RData')){
-            load(file='wkspace_lo.RData')
-    }
-    if (!("solution_lo" %in% ls())){
-            dataFile = 'Data/Sub_391_2_pool_22adb_hg19_SOX2_plus__foregut_endoderm__32a860ee85db9ac9/genes.no_mt.ec.tab' 
-        high_var_data = get_high_var_genes(dataFile=dataFile, sd_cv_thresh=500)
-            c(solution_lo,dist_lo,clust_lo)=run_lo_lpwc(dat=high_var_data, timepoints = 0:96)
-    }
-    if (!("sol_tpm_lo" %in% ls())){
-        dataFile = 'Data/Sub_391_2_pool_22adb_hg19_SOX2_plus__foregut_endoderm__32a860ee85db9ac9/genes.no_mt.tpm.rescale.tab' 
-        high_var_data = get_high_var_genes(dataFile=dataFile, sd_cv_thresh=500)
-        c(sol_tpm_lo,dist_tpm_lo,clust_tpm_lo)=run_lo_lpwc(dat=high_var_data, timepoints = 0:96)
-    }
-    save.image(file='wkspace_lo.RData')
-    print("finished.")
-}
-
 get_h_clust <- function(solution, dat){
     distm <- as.matrix(1 - solution$corr)
     rownames(distm) = rownames(dat)
     dist = as.dist(distm)
     return(hclust(dist))
 }
+
+
+tuner <- function(dataFile, sd_cv_thresh){
+    # for TPM, 40 gets me all 6 with only 458 tot
+    # for EC, 150 gets me all 6 with 1428 tot
+    genes_of_interest = c('T', 'MIXL1', 'EOMES', 
+                          'SOX17', 'CXCR4', 'GATA6')
+    dat = get_high_var_genes(dataFile=dataFile, sd_cv_thresh=sd_cv_thresh)
+    n = 0
+    for (i in seq_along(genes_of_interest)){
+        if (genes_of_interest[i] %in% rownames(dat)){
+            n = n+ 1
+            print(genes_of_interest[i])
+        }
+    }
+    print(c(n, dim(dat)))
+}
+
 
 # groups = cutree(clust, k=10)
 # solution_lo = LPWC::corr.bestlag(top_var, timepoints = timepoints, max.lag = 20, penalty = "low", iter = 10)
